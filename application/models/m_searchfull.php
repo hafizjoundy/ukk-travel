e4ebaf5b212e1e33434ef8891535177f02b68b26
anikaputriu <anikaputriutami@gmail.com>
2018-02-13 15:13:37 +0700

    refresh

--- application/models/m_searchfull.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Class M_searchfull extends CI_Model{
-    public function search_rute($data){
-        $this->db->where($data); //where from $data array
-        $query = $this->db->get('rute'); //get db
-        return $query->result_array(); //return as array
-    }
-}
